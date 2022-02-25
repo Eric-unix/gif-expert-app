bd3bc90a61aa15e8fe6297448ee0e8e67acc4253
EricUnix <erixlopez6@gmail.com>
2022-02-25 17:57:45 -0600

    comunicacion entre componentes

--- src/components/AddCategory.js
+++ src/components/AddCategory.js
@@ -1,10 +1,19 @@
 import React from "react"
+import PropType from 'prop-types';
+
 export const AddCategory = () => {
 
     const [inputValue, setInpputValue] = useState('Hola mundo')
 
     const handleInputChange = (e) => {
         setInpputValue(e.target.value);
+    }
+
+    const handleSubmiy = (e) =>{
+        e.preventDefault();
+
+        if (inputValue( cats => [...cats, inputValue]));
+        setInpputValue('');
     }
 
 
@@ -17,4 +26,10 @@ export const AddCategory = () => {
                 />      
         </form>
     )
+}
+
+
+
+AddCategory.PropType = {
+    setCategories: PropType.func.isRequired
 }
