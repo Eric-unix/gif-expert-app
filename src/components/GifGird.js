d4df420dd5b0591ab7f00442cc2e88de3f51328f
EricUnix <erixlopez6@gmail.com>
2022-02-25 18:25:54 -0600

    useEffect

--- src/components/GifGird.js
+++ src/components/GifGird.js
@@ -1,8 +1,14 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 export const GifGird = ({category}) => {
 
-    const getGifts = () =>{
+    const [count, setCount] = useState(0);
+
+    useEffect( () =>{
+        getGifs();
+    }, [])
+
+    const getGifs = () =>{
 
         const url = 'api.giphy.com/v1/gifs/search?api_key=4029qNgQvECA5PhaTUJBZbGLQVpiWSxv&q=Rick and Morty&limit =10'
         const resp = await fetch (url);
@@ -24,6 +30,8 @@ export const GifGird = ({category}) => {
   return (
     <div>
         <h3>{category}</h3>
+        <h3>{count} </h3>
+        <button onClick={ () => setcount (count +1)}></button>
 
     </div>
   )
